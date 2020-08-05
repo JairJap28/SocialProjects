25891bf26797fc314a46adcc9193f4a345a09271
Jair Jimenez Munevar <jairmunevar@gmail.com>
2020-08-04 20:06:35 -0500

    Show projects from Firestore
    - Map Firestore project's collection to state.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -17,7 +17,7 @@ class Dashboard extends Component {
             <div className="dashboard container">
                 <div className="row">
                     <div className="col s12 m6">
-                        <ProjectList projects={projects}/>
+                        <ProjectList projects={projects || []}/>
                     </div>
                     <div className="col s12 m5 offset-m1">
                         <Notifications />
@@ -29,7 +29,7 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => ({
-    projects: state.project.projects
+    projects: state.firestore.ordered.projects
 })
 
 export default compose(
